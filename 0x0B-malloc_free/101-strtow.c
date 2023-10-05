1deb39ca6dfcb868bc7211fb3f58b0b6c96e6a49
esraasay <esraa.krobeen@gmail.com>
2023-10-04 18:43:24 -0700

    a

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,14 +1,15 @@
-#include <stdlib.h>
 #include "main.h"
+#include <stdlib.h>
+
 /**
  * count_word - helper function to count the number of words in a string
- * @s: string to be evaluated
+ * @s: string to evaluate
  *
- * Returns: number of words
+ * Return: number of words
  */
 int count_word(char *s)
 {
-int f, c, w;
+	int f, c, w;
 
 	f = 0;
 	w = 0;
@@ -27,53 +28,3 @@ int f, c, w;
 	return (w);
 }
 /**
- * **strtow - splits a string into words
- * @str: string to be decomposed
- *
- * Return: pointer to an array of strings (success)
- * or NULL (error)
- */
-
-
-char **strtow(char *str)
-{
-	char **m, *t;
-	int i, k = 0, len = 0, words, c = 0, start, end;
-
-	while (*(str + len))
-		len++;
-	words = count_word(str);
-	if (words == 0)
-		return (NULL);
-
-	m = (char **) malloc(sizeof(char *) * (words + 1));
-	if (m == NULL)
-		return (NULL);
-
-	for (i = 0; i <= len; i++)
-	{
-		if (str[i] == ' ' || str[i] == '\0')
-		{
-			if (c)
-			{
-				end = i;
-				t = (char *) malloc(sizeof(char) * (c + 1));
-				if (t == NULL)
-					return (NULL);
-				while (start < end)
-					*t++ = str[start++];
-				*t = '\0';
-				m[k] = tmp - c;
-				k++;
-				c = 0;
-			}
-		}
-		else if (c++ == 0)
-			start = i;
-	}
-
-	m[k] = NULL;
-
-	return (m);
-}
-
