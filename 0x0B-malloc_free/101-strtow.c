d021b916369bdea6f8c4c5519fe19f41dcac4628
esraasay <esraa.krobeen@gmail.com>
2023-10-04 18:53:42 -0700

    a

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -3,9 +3,9 @@
 
 /**
  * count_word - helper function to count the number of words in a string
- * @s: string to evaluate
+ * @s: string to Evaluate
  *
- * Return: number of words
+ * Return: num of words
  */
 int count_word(char *s)
 {
@@ -27,4 +27,54 @@ int count_word(char *s)
 
 	return (w);
 }
+
+
 /**
+ * **strtow - splits a string into Words
+ * @str: string to split
+ *
+ * Return: Pointer to an array of strings (Success)
+ * or NULL (Error)
+ */
+char **strtow(char *str)
+{
+	char **m, *p;
+	int i, k = 0, len = 0, words, c = 0, start, end;
+
+	while (*(str + len))
+		len++;
+	words = count_word(str);
+	if (words == 0)
+		return (NULL);
+
+	m = (char **) malloc(sizeof(char *) * (words + 1));
+	if (m == NULL)
+		return (NULL);
+
+	for (i = 0; i <= len; i++)
+	{
+		if (str[i] == ' ' || str[i] == '\0')
+		{
+			if (c)
+			{
+				end = i;
+				p = (char *) malloc(sizeof(char) * (c + 1));
+				if (p == NULL)
+					return (NULL);
+				while (start < end)
+					*tmp++ = str[start++];
+				*p = '\0';
+				m[k] = p - c;
+				k++;
+				c = 0;
+			}
+		}
+		else if (c++ == 0)
+			start = i;
+	}
+
+	m[k] = NULL;
+
+	return (m);
+}
+
