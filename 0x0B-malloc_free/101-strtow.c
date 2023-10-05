7e897116d330b54448a673d8f035532527c15f93
esraasay <esraa.krobeen@gmail.com>
2023-10-04 18:38:03 -0700

    a

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -8,18 +8,18 @@
  */
 int count_word(char *s)
 {
-int flag, c, w;
+int f, c, w;
 
-	flag = 0;
+	f = 0;
 	w = 0;
 
 	for (c = 0; s[c] != '\0'; c++)
 	{
 		if (s[c] == ' ')
-			flag = 0;
-		else if (flag == 0)
+			f = 0;
+		else if (f == 0)
 		{
-			flag = 1;
+			f = 1;
 			w++;
 		}
 	}
