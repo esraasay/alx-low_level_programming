2c3568094a263821d5a9544830141d7fba586640
esraasay <esraa.krobeen@gmail.com>
2023-10-09 17:30:55 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,72 +1,21 @@
 #include "main.h"
 #include <stdlib.h>
-#include <stdio.h>
-
-#define ERR_MSG "Error"
-
-
 /**
- * is_digit - checks if a string contains a non-digit char
- * @s: string to be Evaluated.
- *
- * Return: 0 if no-digit is found, 1 otherwise
- */
-
-
-int is_digit(char *s)
+* main -  a program that multiplies two positive numbers
+* @argc: the number of arguments
+* @argv: an array of strings containing each argument
+* Return: returns 0 (success)
+*/
+int main(int argc, char **argv)
 {
-	int k = 0;
-
-	while (s[k])
-	{
-		if (s[k] < '0' || s[k] > '9')
-			return (0);
-		k++;
-	}
-	return (1);
-}
-
-
-/**
- * _strlen - returns the length of a string
- * @s: string to evaluate.
- *
- * Return: Length of the String
- */
-
-
-
-
-int _strlen(char *s)
-{
-	int m = 0;
-
-	while (s[m] != '\0')
-	{
-		m++;
-	}
-	return (m);
-}
-
-/**
- * main - multiplies two positive Numbers
- * @argc: Number of arguments
- * @argv: Array of arguments
- *
- * Return: always 0 (success)
- */
-
-
-int main(int argc, char *argv[])
-{
-	char *z1, *z2;
+	char *s1, *s2;
 	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
-	z1 = argv[1], z2 = argv[2];
-	if (argc != 3 || !is_digit(z1) || !is_digit(z2))
+	s1 = argv[1], s2 = argv[2];
+	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
 		errors();
-	len1 = _strlen(z1);
-	len2 = _strlen(z2);
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
 	len = len1 + len2 + 1;
 	result = malloc(sizeof(int) * len);
 	if (!result)
@@ -75,11 +24,11 @@ int main(int argc, char *argv[])
 		result[i] = 0;
 	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
-		digit1 = z1[len1] - '0';
+		digit1 = s1[len1] - '0';
 		carry = 0;
-		for (len2 = _strlen(z2) - 1; len2 >= 0; len2--)
+		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
 		{
-			digit2 = z2[len2] - '0';
+			digit2 = s2[len2] - '0';
 			carry += result[len1 + len2 + 1] + (digit1 * digit2);
 			result[len1 + len2 + 1] = carry % 10;
 			carry /= 10;
@@ -101,3 +50,45 @@ int main(int argc, char *argv[])
 	return (0);
 }
 
+/**
+* is_digit - checks if every character of a string is a digit
+* @s: the string to be checked
+* Return: returns 0 if a non-digit is found, 1 otherwise
+*/
+int is_digit(char *s)
+{
+	int i = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] < '0' || s[i] > '9')
+			return (0);
+	}
+	return (1);
+}
+
+/**
+* _strlen - calculates the length of a string (excluding the null-terminator)
+* @s: the string to be checked
+* Return: returns the length of the string as an int
+*/
+int _strlen(char *s)
+{
+	int d = 0, r = 0;
+
+	for (d = 0; s[d] != '\0'; d++)
+		r++;
+
+	return (r);
+}
+
+/**
+* errors - prints out "Error" if an error is encountered and exits with
+* a status of 98
+* Return: returns nothing
+*/
+void errors(void)
+{
+	printf("Error\n");
+	exit(98);
+}
