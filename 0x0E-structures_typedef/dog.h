766b3cb1be2517f0e128ff4b8aee170c433a3695
esraasay <esraa.krobeen@gmail.com>
2023-10-10 17:36:02 -0700

    0x0E-structures_typedef/

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+
+/**
+ * struct dog - a dog's basic Info
+ * @name: First Member
+ * @age: Second Member
+ * @owner: Third Member
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
+
