ac7e202ffea8af0f3598fe7b2f02953264c069c0
esraasay <esraa.krobeen@gmail.com>
2023-10-19 20:00:34 -0700

    1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -8,13 +8,13 @@
  */
 size_t list_len(const list_t *h)
 {
- size_t s = 0;
+size_t s = 0;
 
- while (h)
- {
- s++;
- h = h->next;
- }
- return (s);
+while (h)
+{
+s++;
+h = h->next;
+}
+return (s);
 }
 
